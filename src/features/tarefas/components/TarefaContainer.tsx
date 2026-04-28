71488760d4c54677bf616a928057b748121fc59f
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-28 10:42:36 -0300

    feat(todos): adiciona estrutural do TarefaFiltro dentro do TarefaContainer

--- src/features/tarefas/components/TarefaContainer.tsx
+++ src/features/tarefas/components/TarefaContainer.tsx
@@ -1,11 +1,17 @@
 import TituloGenericoTarefa from "./TituloGenericoTarefa";
 import { frasesMotivacionais } from "../mocks/frasesMotivacionais.mock";
 import TarefaInput from "./TarefaInput";
-export default function TarefaContainer(){
+import TarefaFiltro from "./TarefaFiltro";
+
+export default async function TarefaContainer(){
+    
+
     return(
         <div className="max-w-3xl mx-auto w-full p-6">
             <TituloGenericoTarefa frasesMotivacionais={frasesMotivacionais}/>
             <TarefaInput/>
+            <TarefaFiltro />
+            
         </div>
     )
 }
