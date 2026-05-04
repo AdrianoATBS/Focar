2b9449166825ca8ce08c1e2bfad446ce688aa206
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-04 11:05:52 -0300

    feat(tarefas): implementa toggle e divisão entre pendentes e completadas

--- src/features/tarefas/components/TarefaContainer.tsx
+++ src/features/tarefas/components/TarefaContainer.tsx
@@ -1,17 +1,38 @@
+"use client"
 import TituloGenericoTarefa from "./TituloGenericoTarefa";
 import { frasesMotivacionais } from "../mocks/frasesMotivacionais.mock";
 import TarefaInput from "./TarefaInput";
 import TarefaFiltro from "./TarefaFiltro";
+import ListaDeTarefa from "./ListaDeTarefa";
+import { Todo } from "../types/todos.model";
+import { useState } from "react";
+interface ListaDeTarefaProps {
+    todos: Todo[];
+   
+}
+export default  function TarefaContainer({todos}: ListaDeTarefaProps) {
+    const [lista, setLista] = useState<Todo[]>(todos);
 
-export default async function TarefaContainer(){
-    
+    const toggleTarefa = (id: number) => {
+        const novaLista = lista.map((tarefa) => {
+            return tarefa.id === id ? {...tarefa, completed: !tarefa.completed} : tarefa
+        })
+        setLista(novaLista);
+    };
+
+    const pendentes = lista.filter((tarefa) => !tarefa.completed);
+    const completadas = lista.filter((tarefa) => tarefa.completed);
 
     return(
         <div className="max-w-3xl mx-auto w-full p-6">
             <TituloGenericoTarefa frasesMotivacionais={frasesMotivacionais}/>
             <TarefaInput/>
             <TarefaFiltro />
-            
+          
+            <ListaDeTarefa todos={pendentes} toggleTarefa={toggleTarefa}/>
+
+            <h2 className="text-[0.875rem] text-texto-suave mt-5 mb-5">{completadas.length} Tarefas Completadas</h2>
+            <ListaDeTarefa todos={completadas} toggleTarefa={toggleTarefa}/>
         </div>
     )
 }
