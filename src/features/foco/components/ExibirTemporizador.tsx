145a1bc49b5759bbfe0722c17075af426fc9af44
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-19 15:59:45 -0300

    refactor(foco): ajustar layout e substituir ícones do temporizador

--- src/features/foco/components/ExibirTemporizador.tsx
+++ src/features/foco/components/ExibirTemporizador.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { CiCirclePlus } from "react-icons/ci";
-import { CiCircleMinus } from "react-icons/ci";
-import { IoIosRefresh } from "react-icons/io";
+import { FaPlus } from "react-icons/fa6";
+import { FiMinus } from "react-icons/fi";
+import { SlRefresh } from "react-icons/sl";
+
 
 interface ExibirTemporizadorProps {
     tempo: number;
@@ -18,37 +19,44 @@ export default function ExibirTemporizador({ tempo, formatarTempo,
     return(
         <section className="flex flex-col items-center justify-center border-8 border-primaria-escura
             rounded-full 
-            w-84 h-84">
+            w-72 h-72 relative">
 
-            <div className="flex flex-col items-center justify-center relative">
+
+            <div className="flex flex-col items-center justify-center absolute">
                 <span className="text-6xl font-bold">{formatarTempo(tempo)}</span>
+                <div className=" border-t-4 border-primaria-clara w-8 rounded-2xl m-1"></div>
+                <p className="texto-suave">Minutos Restantes</p>
             </div>
-            <div className=" relative">
-                <div className="absolute bottom-[-35] left-38 flex flex-col items-center justify-center gap-6">
-                    <button  onClick={resetarTempo}
-                    className="bg-superficie shadow-lg p-2 rounded-full m-2 absolute 
-                   cursor-pointer active:scale-95 flex "
-                    >
-                        <IoIosRefresh />
-                    </button>
+            
+            <div className=" absolute -right-10 top-1/2 transform -translate-y-1/2
+            ">
+                <div className="flex flex-col items-end justify-end">
 
                     <button 
                     onClick={adicionarTempo}
                     className="bg-superficie shadow-lg text-branca p-2 rounded-full m-2
                     cursor-pointer active:scale-95">
                         
-                        <CiCirclePlus />
+                        <FaPlus  className="text-[#151B2D]"/>
+                    </button>
+
+                    <button  onClick={resetarTempo}
+                    className="bg-superficie shadow-lg p-2 rounded-full m-2  
+                    cursor-pointer active:scale-95 flex "
+                    >
+                        <SlRefresh className="text-[#151B2D]" />
                     </button>
 
                     <button 
-                      onClick={diminuirTempo}
+                    onClick={diminuirTempo}
                     className="bg-superficie shadow-lg text-branca p-2 rounded-full m-2 
                     cursor-pointer active:scale-95">
-                         <CiCircleMinus />
+                        <FiMinus className="text-[#151B2D]" />
                     </button>
                 </div>
 
             </div>
+            
             
         </section>
     )
