c3fa764cc03d6ac32ffd8d65bfa22b26703add28
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-06-18 11:05:37 -0300

    feat(shared): destaca item ativo da navegação conforme a rota atual

--- src/shared/components/Sidebar.tsx
+++ src/shared/components/Sidebar.tsx
@@ -1,35 +1,38 @@
-import Image from "next/image";
+"use client"
 import { MdWindow } from "react-icons/md";
 import { MdOutlineTimer } from "react-icons/md";
 import { CiCalendar } from "react-icons/ci";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar(){
+    const rotaAtual = usePathname();    
+
+    const EhRotaAtualAtiva = (rota: string) => {
+        return rotaAtual === rota;
+    }
     return(
-        <section className="p-5 max-w-40 h-full border-r border-borda/25 flex flex-col ">
-                <div className="flex items-center mb-6">
-
-                    <div className="bg-linear-to-br from-primaria-escura to-primaria-clara  
-                     w-8 h-8 rounded-xl flex items-center justify-center" >
-                        <Image src="/Icon/janela.png" alt="Janela" width={18} height={18}  
-                        className="w-4 h-4"/>
-                    </div>
-                    <h3 className="titulo-3 pl-2">Focar</h3>
+        <section className="p-5 max-w-40 h-full  flex flex-col ">    
+            <div className="flex flex-col gap-4 pt-4">
+                <div className={`flex items-center gap-2
+                     hover:text-primaria-escura cursor-pointer p-2 rounded-lg
+                     ${EhRotaAtualAtiva('/') ? 'text-[#047857] bg-[#ECFDF5] font-bold' : ''}`}>
+                    <MdWindow className=""/>
+                    <Link href="/" className="">Tarefas</Link>
                 </div>
-
-                <div className="flex flex-col gap-4 pt-4 border-t border-borda/25">
-                    <div className="flex gap-2 hover:text-primaria-escura cursor-pointer">
-                        <MdWindow className=""/>
-                        <h3 className="text-sm font-bold">Tarefas</h3>
-                    </div>
-                    <div className="flex gap-2 hover:text-primaria-escura cursor-pointer">
-                        <MdOutlineTimer />
-                        <h3 className=" text-sm font-bold ">Focus</h3>
-                    </div>
-                    <div className="flex gap-2 hover:text-primaria-escura cursor-pointer ">
-                        <CiCalendar/>
-                        <h3 className="text-sm font-bold ">Calendario</h3>
-                    </div>
+                <div className={`flex items-center gap-2
+                     hover:text-primaria-escura cursor-pointer p-2 rounded-lg
+                     ${EhRotaAtualAtiva('/foco') ? 'text-[#047857] bg-[#ECFDF5] font-bold' : ''}`}>
+                    <MdOutlineTimer />
+                    <Link href="/foco" className="text-sm font-bold">Focus</Link>
                 </div>
+                <div className={`flex items-center gap-2
+                     hover:text-primaria-escura cursor-pointer p-2 rounded-lg
+                     ${EhRotaAtualAtiva('/calendario') ? 'text-[#047857] bg-[#ECFDF5] font-bold' : ''}`}>
+                    <CiCalendar/>
+                    <Link href="/calendario" className="text-sm font-bold ">Calendario</Link>
+                </div>
+            </div>
         </section>
     )
 }
