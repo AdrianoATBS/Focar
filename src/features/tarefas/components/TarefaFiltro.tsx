2fbf2f46952d19a2a8ebac0a0df272ea294bd8d6
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-08-03 10:54:23 -0300

    refactor(tarefa): transforma filtros em lista dinâmica

--- src/features/tarefas/components/TarefaFiltro.tsx
+++ src/features/tarefas/components/TarefaFiltro.tsx
@@ -1,40 +1,33 @@
 "use client";
 import BotaoGenerico from "../../../shared/components/BotaoGenerico";
+export type FiltroTarefa = "todas" | "pendentes" | "concluidas";
+
 interface TarefaFiltroProps {
-   filtro: string;
-   setFiltro: (filtro: string) => void;
+   filtro: FiltroTarefa;
+   setFiltro: (filtro: FiltroTarefa) => void;
 }
 export default function TarefaFiltro({ filtro, setFiltro }: TarefaFiltroProps) {
-    
+    const filtros =[
+        {label: "Todas", value: "todas"},
+        {label: "Pendentes", value: "pendentes"},
+        {label: "Concluídas", value: "concluidas"}
+    ]
 
     return(
         <div className="mt-7 flex justify-start items-end border border-borda gap-4
         bg-[#F2F4F6] rounded-full p-2 w-fit shadow-lg mb-8">
-            <BotaoGenerico 
-            texto="Todas" 
-            onClick={() => setFiltro("todas")}
-
-            className={`cursor-pointer p-2
-            ${filtro === "todas" ? 
-            'bg-superficie text-primaria rounded-full shadow-lg' : 
-            'text-texto-suave'}`}/>
-
-            <BotaoGenerico 
-            texto="Pendentes" 
-            onClick={() => setFiltro("pendentes") }
-            className={` cursor-pointer p-2 
-            ${filtro === "pendentes" ? 
+            {filtros.map((filtroItem) => (
+                <BotaoGenerico 
+                key={filtroItem.value}
+                texto={filtroItem.label} 
+                onClick={() => setFiltro(filtroItem.value as FiltroTarefa)}
+                className={`cursor-pointer p-2
+                ${filtro === filtroItem.value ? 
                 'bg-superficie text-primaria rounded-full shadow-lg' : 
-            'text-texto-suave'}`}/>
-
-            <BotaoGenerico 
-            texto="Concluídas" 
-            onClick={() => setFiltro("concluidas") }
-            className={` cursor-pointer p-2
-            ${filtro === "concluidas" ? 
-            ' rounded-full bg-superficie text-primaria shadow-lg' : 
-            'text-texto-suave'}`}/>
-            
+                'text-texto-suave'}`}
+                />
+            ))}
         </div>
+        
     )
 }
